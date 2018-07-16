9e27ed354263e6a1d60a1ff8ec6dbc497643a851
zhxh <zhxhcoder@gmail.com>
2018-07-16 14:02:00 +0800

    XEditText

---
+++ xcomponentlib/src/main/java/com/zhxh/xcomponentlib/XEditText.java
@@ -0,0 +1,25 @@
+package com.zhxh.xcomponentlib;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+
+/**
+ * Created by zhxh on 2018/7/16
+ */
+public class XEditText extends AppCompatEditText {
+
+    public XEditText(Context context) {
+        super(context);
+    }
+
+    public XEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public XEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
